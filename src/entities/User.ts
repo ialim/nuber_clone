1d7ab21ef6a8983494080a83109a668a89540f1b
Alimson Ibrahim <ialimson@gmail.com>
2021-01-17 17:00:34 +0100

    Entities completed

---
+++ src/entities/User.ts
@@ -0,0 +1,111 @@
+import {} from "class-validator";
+import {
+  BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import bcrypt from "bcrypt";
+import Chat from "./Chat";
+import Message from "./Message";
+import Ride from "./Ride";
+import Verification from "./Verification";
+
+const BCRYPT_ROUNDS = 10;
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text", unique: true })
+  email: string;
+
+  @Column({ type: "boolean", default: false })
+  verifiedEmail: boolean;
+
+  @Column({ type: "text" })
+  firstName: string;
+
+  @Column({ type: "text" })
+  lastName: string;
+
+  @Column({ type: "int" })
+  age: number;
+
+  @Column({ type: "text" })
+  password: string;
+
+  @Column({ type: "text" })
+  phoneNumber: string;
+
+  @Column({ type: "boolean", default: false })
+  verifiedPhoneNumber: boolean;
+
+  @Column({ type: "text" })
+  profilePhoto: string;
+
+  @Column({ type: "boolean", default: false })
+  isDriving: boolean;
+
+  @Column({ type: "boolean", default: false })
+  isRiding: boolean;
+
+  @Column({ type: "boolean", default: false })
+  isTaken: boolean;
+
+  @Column({ type: "double precision", default: 0 })
+  lastLng: number;
+
+  @Column({ type: "double precision", default: 0 })
+  lastLat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  lastOrientation: number;
+
+  @ManyToOne((type) => Chat, (chat) => chat.participants)
+  chat: Chat;
+
+  @OneToMany((type) => Message, (message) => message.user)
+  messages: Message[];
+
+  @OneToMany((type) => Ride, (ride) => ride.passenger)
+  ridesAsPassanger: Ride[];
+
+  @OneToMany((type) => Ride, (ride) => ride.driver)
+  ridesAsDriver: Ride[];
+
+  @OneToMany((type) => Verification, (verification) => verification.user)
+  verificattions: Verification[];
+
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  public comparePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if (this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  }
+
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUNDS);
+  }
+}
+
+export default User;
